b654aac582b876f0762307be2eef10d03fee221d
sayroahm <sakea1221@gmail.com>
2026-03-08 17:23:15 -0400

    Create copyright.js

---
+++ js/copyright.js
@@ -0,0 +1,2 @@
+console.log("National Bureau of Exploitation Protection© 2026-2027. All rights & assets remain reserved to NBEP.");
+console.log("For questions & concerns regarding copyright & trademarks reserved to NBEP, please contact us via the path /contact-us");
